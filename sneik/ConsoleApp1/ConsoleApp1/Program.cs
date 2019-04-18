09f42c797aaec43c7d834f92f1ae2d63024a3871
vevenchick <vova.stoev5@gmail.com>
2019-04-18 18:27:59 +0300

    move fruite and snake coordinates to Game state.

--- sneik/ConsoleApp1/ConsoleApp1/Program.cs
+++ sneik/ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,29 +8,35 @@ namespace ConsoleApp1
 {
     class Game
     {
-        int _w; int _h;
-        Random r = new Random();
+        int _w;
+        int _h;
+        int _frute_X;
+        int _frute_Y;
+        int _snake_X;
+        int _snake_Y;
+        Random random = new Random();
+
+
         public Game(int w, int h)
         {
             _w = w;
             _h = h;
-
+            _frute_X = random.Next(1, _w - 2);
+            _frute_Y = random.Next(1, _h - 2);
+            _snake_X = random.Next(1, _w - 2);
+            _snake_Y = random.Next(1, _h - 2);
         }
         public void DrawFruit()
         {
-            int x = r.Next(1, _w - 1);
-            int y = r.Next(1, _h);
-            Console.SetCursorPosition(x, y);
-            Console.Write("#");
+            Console.SetCursorPosition(_frute_X, _frute_Y);
+            Console.Write("Q");
         }
-        public void DrawSneik()
+        public void DrawSnake()
         {
-
-            int x = r.Next(1, _w - 1);
-            int y = r.Next(1, _h);
-            Console.SetCursorPosition(x, y);
-            Console.Write("O");
+            Console.SetCursorPosition(_snake_X, _snake_Y);
+            Console.Write("R");
         }
+
         public void DrawFeild()
         {
 
@@ -40,8 +46,8 @@ namespace ConsoleApp1
                 Console.Write("#");
                 Console.SetCursorPosition(i, _h);
                 Console.Write("#");
-            }
 
+            }
             for (int c = 1; c < _h; c++)
             {
                 Console.SetCursorPosition(0, c);
@@ -54,43 +60,44 @@ namespace ConsoleApp1
     }
     class Program
     {
+
+
+
         static void Main(string[] args)
         {
             string n = Console.ReadLine();
             string[] mass = n.Split(' ');
             int w = Int32.Parse(mass[0]);
             int h = Int32.Parse(mass[1]);
-            Console.SetCursorPosition(0, h + 1);
             ConsoleKeyInfo key = new ConsoleKeyInfo();
             Game game = new Game(w, h);
             while (true)
             {
                 Console.Clear();
-
                 game.DrawFeild();
                 game.DrawFruit();
-                game.DrawSneik();
+                game.DrawSnake();
+                Console.SetCursorPosition(0, h + 1);
                 switch (key.Key)
                 {
                     case ConsoleKey.UpArrow:
-                        Console.Write("Sneak up");
+                        Console.WriteLine("змея вверх");
                         break;
                     case ConsoleKey.DownArrow:
-
-                        Console.Write("Sneak down");
+                        Console.WriteLine("змея вниз");
                         break;
                     case ConsoleKey.LeftArrow:
-
-                        Console.Write("Sneak left");
+                        Console.WriteLine("змея влево");
                         break;
                     case ConsoleKey.RightArrow:
-
-                        Console.Write("Sneak right");
+                        Console.WriteLine("змея вправо");
                         break;
-
                 }
                 key = Console.ReadKey();
+
+
             }
+
         }
     }
 }
